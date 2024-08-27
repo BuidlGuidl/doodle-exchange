91ae629150a9384c51d003bdeb9da6bc969a8e81
Sherkhan Azimov <sherkhanazim@gmail.com>
2024-08-28 00:22:00 +0500

    fix: add firebase info to config

--- packages/nextjs/doodle.config.ts
+++ packages/nextjs/doodle.config.ts
@@ -2,6 +2,14 @@ const doodleConfig = {
   mongodb_uri: "mongodb+srv://test:DkqcaDD9inDunDFx@cluster0.uh2ayxo.mongodb.net/?retryWrites=true&w=majority",
   ably_api_key: "Fbq6sA.xC_GgQ:a9uQJKCunyvMmh1nVvcZaZFuZw_2LYbcHvTno5uPV5c",
   jwt_secret: "hardsecret",
+  FIREBASE_API_KEY: "AIzaSyBVKXBPp9SV8-BmTFk--X5hzAd0L4HwObw",
+  FIREBASE_AUTH_DOMAIN: "imagedrawings-14c52.firebaseapp.com",
+  FIREBASE_AUTH_PROJECT_ID: "imagedrawings-14c52",
+  FIREBASE_STORAGE_BUCKET: "imagedrawings-14c52.appspot.com",
+  FIREBASE_MESSAGING_SENDER_ID: "1059342694708",
+  FIREBASE_APP_ID: "1:1059342694708:web:2976a57a4d91f35ac1cd61",
+  FIREBASE_USER_EMAIL: "aim.testnets@gmail.com",
+  FIREBASE_USER_PASSWORD: "mHn1l9,9~|T&",
 };
 
 export default doodleConfig;
