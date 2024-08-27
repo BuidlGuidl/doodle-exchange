60b237d32cde48111f3be1ad4e6961902ff90165
Sherkhan Azimov <sherkhanazim@gmail.com>
2024-08-27 09:33:10 +0500

    fix: code refactoring

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -7,49 +7,35 @@ import SingleGame from "./_components/SingleGame";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 
+type gameStateType = "createGame" | "joinGame" | "singleGame";
+
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
   const [inviteCode, setInviteCode] = useState("");
-  const [gameState, setGameState] = useState<"createGame" | "joinGame" | "singleGame">("joinGame");
+  const [gameState, setGameState] = useState<gameStateType>("joinGame");
+
+  const menuItems = [
+    { state: "joinGame", label: "Join Game" },
+    { state: "createGame", label: "Start Game" },
+    { state: "singleGame", label: "Single Game" },
+  ];
 
   return (
     <>
       <div className="mx-auto mt-5 p-6 flex flex-col">
         <div className="flex justify-center">
           <ul className="menu menu-horizontal justify-center p-2 bg-base-300 rounded-full mb-8 w-fit mx-auto">
-            <li onClick={() => setGameState("joinGame")}>
-              <a
-                className={
-                  gameState == "joinGame"
-                    ? "bg-accent px-3 rounded-full py-1 cursor-pointer "
-                    : "px-3 rounded-full py-1 cursor-pointer hover:bg-base-100"
-                }
-              >
-                Join Game
-              </a>
-            </li>
-            <li onClick={() => setGameState("createGame")}>
-              <a
-                className={
-                  gameState == "createGame"
-                    ? "bg-accent px-3 rounded-full py-1 cursor-pointer  transition ease-in-out delay-150"
-                    : "px-3 rounded-full py-1 cursor-pointer hover:bg-base-100"
-                }
-              >
-                Start Game
-              </a>
-            </li>
-            <li onClick={() => setGameState("singleGame")}>
-              <a
-                className={
-                  gameState == "singleGame"
-                    ? "bg-accent px-3 rounded-full py-1 cursor-pointer  transition ease-in-out delay-150"
-                    : "px-3 rounded-full py-1 cursor-pointer hover:bg-base-100"
-                }
-              >
-                Single Game
-              </a>
-            </li>
+            {menuItems.map(item => (
+              <li key={item.state} onClick={() => setGameState(item.state as gameStateType)}>
+                <div
+                  className={`px-3 rounded-full py-1 cursor-pointer ${
+                    gameState === item.state ? "bg-accent" : "hover:bg-base-100"
+                  } transition ease-in-out delay-150`}
+                >
+                  {item.label}
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
 
